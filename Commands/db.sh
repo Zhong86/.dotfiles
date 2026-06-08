5a9b7fb32fa8ef8ec9b870b98a1f15c785809bec
Zhong86 <billy3rlvin@gmail.com>
2026-06-08 22:00:28 +0700

    feat: db for mariaDB + postgres

--- Commands/db.sh
+++ Commands/db.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 if [[ -z "$1" || $1 == 'start' ]]; then
-  echo "Starting servers: MariaDB"
+  echo "Starting servers: MariaDB, PostgreSQL"
   sudo systemctl start mariadb
+  sudo systemctl start postgresql
 elif [[ $1 == 'stop' ]]; then
-  echo "Stopping servers"
+  echo "Stopping servers: MariaDB, PostgreSQL"
   sudo systemctl stop mariadb
+  sudo systemctl stop postgresql
+elif [[ $1 == '--maria' ]]; then
+  echo "Starting server: MariaDB"
+  sudo systemctl start mariadb
+elif [[ $1 == '--pg' ]]; then
+  echo "Starting server: PostgreSQL"
+  sudo systemctl start postgresql
 else
-  echo "Available params: start, stop"
+  echo "Available params: start, stop, --maria, --pg"
 fi
